9906240fc92c36c08940051a804406ee8844e041
santiagonoguez <santiago@nul-unu.com>
2018-01-19 07:53:58 -0600

    correct webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,30 +5,27 @@ const path = require("path");
 const UnzipsfxPlugin = require("unzipsfx-webpack-plugin");
 const ZipPlugin = require("zip-webpack-plugin");
 
-module.exports = env => {
-
-  return {
-    entry: "./src/index.js",
-    target: "node",
-    externals: [nodeExternals()],
-    output: {
-      path: path.join(__dirname, "build", "licensing"),
-      filename: "index.js"
-    },
-    plugins: [
-      new CopyWebpackPlugin([
-        {from: "./build-temp/node_modules", to: "node_modules"},
-        {from: "./build-temp/package.json"}
-      ]),
-      new MinifyPlugin(),
-      new ZipPlugin({
-        path: path.join(__dirname, "build"),
-        filename: "licensing"
-      }),
-      new UnzipsfxPlugin({
-        outputPath: path.join(__dirname, "build"),
-        outputFilename: "licensing"
-      })
-    ]
-  };
+module.exports = {
+  entry: "./src/index.js",
+  target: "node",
+  externals: [nodeExternals()],
+  output: {
+    path: path.join(__dirname, "build", "licensing"),
+    filename: "index.js"
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {from: "./build-temp/node_modules", to: "node_modules"},
+      {from: "./build-temp/package.json"}
+    ]),
+    new MinifyPlugin(),
+    new ZipPlugin({
+      path: path.join(__dirname, "build"),
+      filename: "licensing"
+    }),
+    new UnzipsfxPlugin({
+      outputPath: path.join(__dirname, "build"),
+      outputFilename: "licensing"
+    })
+  ]
 };
