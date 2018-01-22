6db022ddf085e1ee00bca264399a34989d355fdc
santiagonoguez <santiago@nul-unu.com>
2018-01-22 08:05:57 -0600

    initialize current table with empty object

--- src/subscriptions.js
+++ src/subscriptions.js
@@ -1,11 +1,13 @@
 const store = require("./store");
 
-let currentSubscriptionStatusTable = null;
+let currentSubscriptionStatusTable = {};
 
 function loadData() {
   // Currently the subscription status come only from store, but in future modules it may also come from the display's GCS bucket.
   return store.getSubscriptionStatusTable()
   .then(table => {
+    // Compare current against previous to see if there are changes; next card
+
     currentSubscriptionStatusTable = table;
 
     // Implement broadcast to other modules here; next card
