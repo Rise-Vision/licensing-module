985910744adaccce73d71ba40326e4cc54dbcbc2
santiagonoguez <santiago@nul-unu.com>
2018-01-30 11:13:48 -0600

    reduce modules webpack build size

--- post-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-cd ./build
-rm -rf licensing
-rm licensing.zip
-cd ..
-rm -rf build-temp
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
-const nodeExternals = require("webpack-node-externals");
 const path = require("path");
 const UnzipsfxPlugin = require("unzipsfx-webpack-plugin");
 const ZipPlugin = require("zip-webpack-plugin");
@@ -8,16 +7,16 @@ const ZipPlugin = require("zip-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   target: "node",
-  externals: [nodeExternals()],
+  externals: {
+    "electron": "electron",
+    "original-fs": "original-fs"
+  },
   output: {
     path: path.join(__dirname, "build", "licensing"),
     filename: "index.js"
   },
   plugins: [
-    new CopyWebpackPlugin([
-      {from: "./build-temp/node_modules", to: "node_modules"},
-      {from: "./build-temp/package.json"}
-    ]),
+    new CopyWebpackPlugin([{from: "package.json"}]),
     new MinifyPlugin(),
     new ZipPlugin({
       path: path.join(__dirname, "build"),
