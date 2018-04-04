e2ac43c05bc77067cc756604dfb9e292df2f3909
santiagonoguez <santiago@nul-unu.com>
2018-04-04 11:30:41 -0500

    remove uneeded manual test script

--- test/manual/deprecated_widget_api_request.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-magic-numbers */
-
-const store = require("../../src/store");
-
-store.fetchRisePlayerProfessionalAuthorization()
-.then(request => console.log(request.body))
-.catch(console.error);
